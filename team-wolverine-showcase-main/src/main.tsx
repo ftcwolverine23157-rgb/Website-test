1fd0245b6680441cc09a96e656b9622e65e15139
ftcwolverine23157-rgb <ftcwolverine23157@gmail.com>
2025-09-02 15:01:58 -0700

    Update main.tsx

--- team-wolverine-showcase-main/src/main.tsx
+++ team-wolverine-showcase-main/src/main.tsx
@@ -2,4 +2,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+
+const link: HTMLLinkElement | null = document.querySelector("link[rel~='icon']");
+if (link) {
+  link.href = "/favicon.ico?v=2"; // points to your Wolverine favicon
+} else {
+  const newLink = document.createElement("link");
+  newLink.rel = "icon";
+  newLink.href = "/favicon.ico?v=2";
+  document.head.appendChild(newLink);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
